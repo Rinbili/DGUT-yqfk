8745255be5a0d8016d6779ad9b2dee1161a85656
Rinbili <41405540+Rinbili@users.noreply.github.com>
2021-12-21 15:52:29 +0800

    Update yqfk.py

--- yqfk.py
+++ yqfk.py
@@ -12,17 +12,17 @@ password = os.environ["PASSWORD"]
 sckey = os.environ["SCKEY"]
 
 def get_page(message, target):
-    url = "https://cas.dgut.edu.cn/home/Oauth/getToken/appid/illnessProtectionHome/state/home.html"
+    url = "https://cas.dgut.edu.cn/home/Oauth/getToken/appid/yqfkdaka/state/home.html"
     session = requests.Session()
     origin = session.get(url=url)
     html = origin.content.decode('utf-8')
     pattern = re.compile(r"var token = \"(.*?)\";", re.MULTILINE | re.DOTALL)
     token_tmp = pattern.search(html).group(1)
-    cookies = {"languageIndex": "0", "last_oauth_appid": "illnessProtectionHome", "last_oauth_state": "home"}
+    cookies = {"languageIndex": "0", "last_oauth_appid": "yqfkdaka" "illnessProtectionHome", "last_oauth_state": "home"}
     data = {'username': username, 'password': password, '__token__': token_tmp, 'wechat_verif': ''}
     headers = {'X-Requested-With': 'XMLHttpRequest'}
     response = session.post(url=url, headers=headers, cookies=cookies, data=data).json()
-
+    
     response_json = json.loads(response)
 
     if response_json['message'] != '验证通过':
@@ -39,13 +39,17 @@ def get_page(message, target):
 
 def post_form(message, target):
     yqfk_session = requests.Session()
+    yqfk_get = urlparse(target[0])
+    data = {}
+    for item in yqfk_get.query.split("&"):
+        data[item.split("=")[0]] = item.split("=", maxsplit=2)[-1]
+    res = yqfk_session.post("https://yqfk-daka-api.dgut.edu.cn/auth", json=data)
     yqfk_acesstoken = yqfk_session.get(url=target[0])
-    pattern = re.compile(r"access_token=(.*?)$", re.MULTILINE | re.DOTALL)
-    access_token = pattern.search(yqfk_acesstoken.url).group(1)
+    access_token = res.json().get('access_token')
     headers_2 = {'authorization': 'Bearer ' + access_token}
     yqfk_session.get(url=yqfk_acesstoken.url)
-    yqfk_info = yqfk_session.get('https://yqfk.dgut.edu.cn/home/base_info/getBaseInfo', headers=headers_2).json()
-    yqfk_json = yqfk_info['info']
+    yqfk_info = yqfk_session.get('https://yqfk-daka-api.dgut.edu.cn/record', headers=headers_2).json()
+    yqfk_json = yqfk_info['user_data']
     yqfk_json['important_area'] = None
     yqfk_json['current_region'] = None
     yqfk_json['acid_test_results'] = None
@@ -53,9 +57,8 @@ def post_form(message, target):
 
     console_msg(yqfk_info['message'])
     message.append(yqfk_info['message'])
-    result = yqfk_session.post(url="https://yqfk.dgut.edu.cn/home/base_info/addBaseInfo", headers=headers_2,
-                               json=yqfk_json).json()
-
+    result = yqfk_session.post(url="https://yqfk-daka-api.dgut.edu.cn/record", headers=headers_2,
+                               json={"data": yqfk_json}).json()
     if 'message' not in result.keys():
         console_msg('提交失败')
         message.append('提交失败')
@@ -64,11 +67,11 @@ def post_form(message, target):
         console_msg(result['message'])
         message.append(result['message'])
 
-        if '已提交' in result['message'] or '成功' in result['message']:
+        if '已经提交' in result['message'] or '成功' in result['message']:
             console_msg('二次提交，确认成功', 0)
             message.append('二次提交，确认成功')
-            result = yqfk_session.post(url="https://yqfk.dgut.edu.cn/home/base_info/addBaseInfo", headers=headers_2,
-                                       json=yqfk_json).json()
+            result = yqfk_session.post(url="https://yqfk-daka-api.dgut.edu.cn/record", headers=headers_2,
+                               json={"data": yqfk_json}).json()
             console_msg(result['message'])
             return 0
         console_msg("二次提交，确认失败", 1)
@@ -113,7 +116,7 @@ def console_msg(msg, level=2):
 
 
 if __name__ == '__main__':
-    console_msg("使用Secret变量")
+    console_msg("开始执行")
     run()
 
     if sckey is None:
